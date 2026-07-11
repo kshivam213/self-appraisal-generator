613fa779b2de50d56b0cbfc3c84da26ed8e3b532
Shivam kumar <shivam.kumar@usemultiplier.com>
2026-07-11 16:05:23 +0530

    Convert to Spring Boot project self-appraisal-generator with health test endpoint

---
+++ src/main/java/com/multiplier/selfappraisal/SelfAppraisalGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.multiplier.selfappraisal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfAppraisalGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfAppraisalGeneratorApplication.class, args);
+    }
+}
+
---
+++ src/test/java/com/multiplier/selfappraisal/SelfAppraisalGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiplier.selfappraisal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfAppraisalGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
